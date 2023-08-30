f0a4c4b9bf2715121a0080061aa89ab38e999c0d
delimmaemma <calvinhtml@gmail.com>
2023-08-30 00:08:05 -0500

    Passes all tests

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,63 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const Car = require('./cars-model')
+const vin = require('vin-validator')
+
+const checkCarId = async (req, res, next) => {
+  try {
+    const car = await Car.getById(req.params.id)
+    if (!car) {
+      next({ status: 404, message: `car with id ${req.params.id} is not found`})
+    } else {
+      req.car = car
+      next()
+    }
+} catch (err) {
+    next(err)
+}
 }
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    let missing = null
+
+    if (!req.body.vin) missing = 'vin'
+    else if (!req.body.make) missing = 'make'
+    else if (!req.body.model) missing = 'model'
+    else if (!req.body.mileage) missing = 'mileage'
+    else {
+      next()
+    }
+  
+    if (missing) next({ status: 400, message: `${missing} is missing`})
+  } catch (err) {
+      next(err)
+  }
+  
 }
 
 const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
+  if (vin.validate(req.body.vin)) {
+    next()
+  } else {
+    next({ status: 400, message: `vin ${req.body.vin} is invalid`})
+  }
 }
 
-const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+const checkVinNumberUnique = async (req, res, next) => {
+  try {
+    const existingVin = await Car.getByVin(req.body.vin)
+    if (!existingVin) {
+      next()
+    } else {
+      next({ status: 400, message: `vin ${req.body.vin} already exists`})
+    }
+  } catch (err) {
+    next(err)
+  }
+}
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
 }
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,29 @@
 // STRETCH
+const cars = [
+    {
+        vin: '19104759182471957',
+        make: "hyundai",
+        model: "elantra",
+        milage: 69420,
+        title: "clean",
+        transmission: "automatic"
+    },
+    {
+        vin: '67230501947280183',
+        make: "toyota",
+        model: "rav4",
+        milage: 128422,
+        transmission: "automatic"
+    },
+    {
+        vin: '6920547296781958',
+        make: "mazda",
+        model: "3",
+        milage: 53920,
+    }
+]
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
